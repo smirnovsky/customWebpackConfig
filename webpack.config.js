6a746fc6b1b32262db5d8d4f0581304e01d2f559
smirnovsky <happy280190@gmail.com>
2022-12-10 13:10:38 +0300

    feat: add filemanager webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const FileManagerPlugin = require("filemanager-webpack-plugin");
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
 
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'index.[contenthash].js',
+    path: path.join(__dirname, "dist"),
+    filename: "index.[contenthash].js",
   },
 
   plugins: [
@@ -14,8 +15,15 @@ module.exports = {
       template: path.join(__dirname, "src", "template.html"),
       filename: "index.html",
     }),
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: ["dist"],
+        },
+      },
+    }),
   ],
-  
+
   devServer: {
     watchFiles: path.join(__dirname, "src"),
     port: 9000,
