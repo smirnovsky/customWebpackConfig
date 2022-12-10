33d8ac432f0515771403e87db700c2ad10bdd20e
smirnovsky <happy280190@gmail.com>
2022-12-10 13:03:46 +0300

    feat: add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,23 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.[contenthash].js',
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'template.html'),
-      filename: 'index.html',
+      template: path.join(__dirname, "src", "template.html"),
+      filename: "index.html",
     }),
   ],
+  
+  devServer: {
+    watchFiles: path.join(__dirname, "src"),
+    port: 9000,
+  },
 };
